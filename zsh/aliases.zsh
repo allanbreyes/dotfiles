ee7a384b17dbb5cf55bf84852aa09cebd8899c1b
Allan Reyes <allan.reyes@robinhood.com>
2018-08-17 16:24:35 -0700

    Update ZSH configs

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -4,7 +4,6 @@ alias r='reload!'
 alias cls='clear' # Good 'ol Clear Screen command
 
 # common commands
-alias ls='ls --color=auto'
 alias l='ls -la'
 alias diff='colordiff'
 alias grep='grep --color=auto'
@@ -20,30 +19,14 @@ alias .3='cd ../../../'
 alias .4='cd ../../../../'
 alias .5='cd ../../../../../'
 
-# safety nets
-alias rm='rm -I --preserve-root'
-alias mv='mv -i'
-alias cp='cp -i'
-alias ln='ln -i'
-alias chown='chown --preserve-root'
-alias chmod='chmod --preserve-root'
-alias chgrp='chgrp --preserve-root'
-
 # miscellaneous aliases
 alias ports='netstat -tulanp'
 alias tree='tree -C --prune'
 alias serve='python -m http.server'
-alias clip='xclip -selection clipboard'
+alias clip='pbcopy'
 
 # shorthand
 alias d='tmux detach'
-alias o='xdg-open'
+alias o='open'
 alias v='vim'
 alias x='exit'
-
-# version managers
-versions() {
-    { echo "\e[1;32mNode.js\e[0m"; nvm version | sed 's/v//'; } | sed -e 'N;s/\n/\t/'
-    { echo "\e[1;34mPython\e[0m"; pyenv version | sed 's/\s\(.*\)//'; } | sed -e 'N;s/\n/\t/'
-    { echo "\e[1;31mRuby\e[0m"; rbenv version | sed 's/\s\(.*\)//'; } | sed -e 'N;s/\n/\t/'
-}
--- zsh/config.zsh
+++ zsh/config.zsh
@@ -39,13 +39,14 @@ setopt complete_aliases
 
 zle -N newtab
 
-bindkey '^[^[[D' backward-word
-bindkey '^[^[[C' forward-word
-bindkey '^[[5D' beginning-of-line
-bindkey '^[[5C' end-of-line
-bindkey '^[[3~' delete-char
-bindkey '^[^N' newtab
-bindkey '^?' backward-delete-char
+# Better history
+# Credits to https://coderwall.com/p/jpj_6q/zsh-better-history-searching-with-arrow-keys
+autoload -U up-line-or-beginning-search
+autoload -U down-line-or-beginning-search
+zle -N up-line-or-beginning-search
+zle -N down-line-or-beginning-search
+bindkey "^[[A" up-line-or-beginning-search # Up
+bindkey "^[[B" down-line-or-beginning-search # Down
 
 zmodload zsh/terminfo
 bindkey "$terminfo[kcuu1]" history-substring-search-up
