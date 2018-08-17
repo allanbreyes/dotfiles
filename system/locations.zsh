fc54dbe4fe4ba0a410a5aaa42218db07c9e51fda
Allan Reyes <allan.reyes@robinhood.com>
2018-08-17 16:19:09 -0700

    Update system config

--- system/locations.zsh
+++ system/locations.zsh
@@ -6,21 +6,21 @@ local cd_or_open() {
   if [[ -d $1 ]]; then
     cd $1
   else
-    xdg-open $1
+    open $1
   fi
 }
 
 docs() {
   cd_or_open "$DOCUMENTS/$1"
 }
-compdef "_files -W '$DOCUMENTS'" docs
+# compdef "_files -W '$DOCUMENTS'" docs
 
 dl() {
   cd_or_open "$DOWNLOADS/$1"
 }
-compdef "_files -W '$DOWNLOADS'" dl
+# compdef "_files -W '$DOWNLOADS'" dl
 
 dt() {
   cd_or_open "$DESKTOP/$1"
 }
-compdef "_files -W '$DESKTOP'" dt
+# compdef "_files -W '$DESKTOP'" dt
